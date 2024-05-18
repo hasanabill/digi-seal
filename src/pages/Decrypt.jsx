32ea7473e27fb177061aa01097859f5d7b703cc1
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-05-18 23:11:02 +0600

    one page responsive

--- src/pages/Decrypt.jsx
+++ src/pages/Decrypt.jsx
@@ -46,7 +46,7 @@ const Decrypt = () => {
 
             <h3 className="text-2xl font-bold mb-4">Decrypt Message</h3>
 
-            <div className='grid grid-cols-2 gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                 <div className="mb-4">
                     <h3 className="text-lg font-bold mb-2">Encrypted Message</h3>
                     <textarea
